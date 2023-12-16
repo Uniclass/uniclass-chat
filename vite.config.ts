3466970f8f47996dcbd6310a6d44e75bb5044ed6
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-12-16 22:41:11 +0700

    chore: update library name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/components/index.ts'),
-			name: '@uniclass-chat',
+			name: 'UniclassChat',
 			fileName: 'uniclass-chat'
 		},
 		rollupOptions: {
