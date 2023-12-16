3b4157ec599acaf65d2d6fce3bb9745c69f1858c
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-12-16 22:07:46 +0700

    feat: update components and package configuration

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ComponentProps } from 'react'
-
-type ButtonProps = ComponentProps<'button'>
-
-export const Button: React.FC<ButtonProps> = ({ ...props }) => {
-	return <button className="text-teal-400" {...props} />
-}
