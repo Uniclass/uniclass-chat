73586e3a38911b066a4cb231478020ee0c600daa
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-12-25 19:29:50 +0700

    feat: update ChatMessage and add new types

--- src/common/types/common.d.ts
+++ src/common/types/common.d.ts
@@ -1,6 +1,6 @@
 type ChatMessage = {
 	id?: string
-	course_id: string
+	room_id: string
 	type: 'MSG' | 'COMP' | 'FILE' | 'NOTI'
 	sender_type: 'TCA' | 'STD' | 'SYS' | 'ADM'
 	sender_id: string
@@ -9,4 +9,22 @@ type ChatMessage = {
 	comp_data?: string
 	skip_audience_type?: string
 	file_url?: string
+}
+
+type RoomAttendan = {
+	firstname: string
+	lastname: string
+	title: string
+	photo_url?: string
+	student_id?: string
+	teacher_id?: string
+}
+
+type ChatRoom = {
+	id: string
+	room_id: string
+	room_name: string
+	room_status: boolean
+	created_time: string
+	updated_time: string
 }
