4c53c3325e2986d7c0c5553d20444920b82cddeb
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-12-25 19:29:37 +0700

    refactor: chat API functions and add new endpoints

--- src/common/api/chat/index.ts
+++ src/common/api/chat/index.ts
@@ -2,20 +2,20 @@ import axios from 'axios'
 
 const fetchChatRoom = (dataBaseApiUrl: string, authToken: string) => {
 	return axios
-		.get(`${dataBaseApiUrl}/chat/room/STD0012`, {
+		.get(`${dataBaseApiUrl}/chat/room`, {
 			headers: {
 				Authorization: `Bearer ${authToken}`
 			}
 		})
 		.then((res) => {
-			if (res.data.code === 200) return res.data.payload
+			if (res.data.code === 200) return res.data.payload as ChatRoom[]
 			throw new Error(res.data.message_ui || res.data.message)
 		})
 }
 
-const fetchChatMessage = (dataBaseApiUrl: string, authToken: string, room_id: string) => {
+const fetchChatMessage = (dataBaseApiUrl: string, authToken: string, room_id: string, ts_st: string, ts_en: string) => {
 	return axios
-		.get(`${dataBaseApiUrl}/chat/message/${room_id || ''}`, {
+		.get(`${dataBaseApiUrl}/chat/room/${room_id}/message?ts_st=${ts_st || ''}&ts_en=${ts_en || ''}`, {
 			headers: {
 				Authorization: `Bearer ${authToken}`
 			}
@@ -26,9 +26,48 @@ const fetchChatMessage = (dataBaseApiUrl: string, authToken: string, room_id: st
 		})
 }
 
-const sendChatMessage = (dataBaseApiUrl: string, authToken: string, body: ChatMessage) => {
+const sendChatMessage = (dataBaseApiUrl: string, authToken: string, body: ChatMessage, room_id: string) => {
 	return axios
-		.post(`${dataBaseApiUrl}/chat/message`, body, {
+		.post(`${dataBaseApiUrl}/chat/room/${room_id}/message`, body, {
+			headers: {
+				Authorization: `Bearer ${authToken}`
+			}
+		})
+		.then((res) => {
+			if (res.data.code === 200) return res.data.payload as ChatMessage
+			throw new Error(res.data.message_ui || res.data.message)
+		})
+}
+
+const getAuthToken = (dataBaseApiUrl: string, authToken: string) => {
+	return axios
+		.get(`${dataBaseApiUrl}/chat/token`, {
+			headers: {
+				Authorization: `Bearer ${authToken}`
+			}
+		})
+		.then((res) => {
+			if (res.data.code === 200) return res.data.payload as string
+			throw new Error(res.data.message_ui || res.data.message)
+		})
+}
+
+const fetchUserProfileList = (dataBaseApiUrl: string, authToken: string, room_id: string) => {
+	return axios
+		.get(`${dataBaseApiUrl}/chat/room/${room_id}/attendant`, {
+			headers: {
+				Authorization: `Bearer ${authToken}`
+			}
+		})
+		.then((res) => {
+			if (res.data.code === 200) return res.data.payload as RoomAttendan[]
+			throw new Error(res.data.message_ui || res.data.message)
+		})
+}
+
+const fetchUserProfile = (dataBaseApiUrl: string, authToken: string, user_id: string) => {
+	return axios
+		.get(`${dataBaseApiUrl}/chat/profile?user_id=${user_id || ''}`, {
 			headers: {
 				Authorization: `Bearer ${authToken}`
 			}
@@ -39,4 +78,4 @@ const sendChatMessage = (dataBaseApiUrl: string, authToken: string, body: ChatMe
 		})
 }
 
-export { fetchChatRoom, fetchChatMessage, sendChatMessage }
+export { fetchChatRoom, fetchChatMessage, sendChatMessage, getAuthToken, fetchUserProfileList, fetchUserProfile }
