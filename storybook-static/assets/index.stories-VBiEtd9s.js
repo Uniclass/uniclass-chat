ddb92772f138d9c4f40a9684b49b27d13bdc3461
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2024-01-04 22:16:03 +0700

    Update script sources in storybook-static

---
+++ storybook-static/assets/index.stories-VBiEtd9s.js
@@ -0,0 +1,5 @@
+import{C as a}from"./index-SJd9sSgM.js";import"./jsx-runtime-vNq4Oc-g.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./card-WIVjaPgj.js";import"./badge-TFhgPZAN.js";import"./createReactComponent-rTvSjvh2.js";import"./index-tvtfaFq4.js";const l={title:"Components/CourseStatus",component:a,parameters:{layout:"centered"},tags:["autodocs"]},t={args:{orderStatus:"B"}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    orderStatus: 'B'
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const S=["Default"];export{t as Default,S as __namedExportsOrder,l as default};
